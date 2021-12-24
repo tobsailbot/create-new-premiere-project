35e2611b19e21a4859f235d7f5043e594db47220
tobsailbot <tobsailbot@gmail.com>
2021-12-24 13:06:08 -0300

    updates and comments

--- new_premiere_project_simpleGUI.py
+++ new_premiere_project_simpleGUI.py
@@ -11,7 +11,7 @@ campos = ['Project Name', 'Project Path', 'Media Folder 1','Media Folder 2','Med
 datos = []
 default = ['',r'D:\TOBI-PC\Descargas\3-VIDEO PROJECTS',r'F:\VIDEO MEDIA\OBS VIDEOS',r'D:\TOBI-PC\Descargas\3-VIDEO PROJECTS\1.VIDEO EDITING RESOURCES\SOUNDS\SOUND FX',r'D:\TOBI-PC\Descargas\3-VIDEO PROJECTS\1.VIDEO EDITING RESOURCES\MEDIA\EMOJIS PNG']
 
-
+# creates a window with multiple input boxes
 box = eg.multenterbox(msg='Enter the name and folders:',title='New Premiere Project',fields=campos, values=default)
 
 
@@ -22,14 +22,16 @@ projDir = Dir+f'\\{projName}'
 print(projName)
 print(projDir)
 
+# creates a new directory 
 os.makedirs(projDir)
 
 print(f'se creo el proyecto: {projName}')
 
-
+# checks for the window title
 win = ahk.find_window(title=b'Adobe Premiere Pro')
 win.activate()
 
+# creates a new project and select the folder
 ahk.send_input('^!n')
 ahk.type(projName)
 ahk.send_input('{Tab}{Tab}')
@@ -39,11 +41,16 @@ ahk.send_input('{Enter}{Enter}')
 ahk.send_input('+{Tab}''+{Tab}''+{Tab}''+{Tab}')
 ahk.send_input('{Enter}')
 
+time.sleep(0.5)
 
+# creates a new sequence
+ahk.send_input('^n')
+ahk.type(projName)
+ahk.send_input('{Enter}')
 
 time.sleep(1)
 
-
+# open the media folders
 subprocess.Popen(f'explorer "{box[2]}"')
 subprocess.Popen(f'explorer "{box[3]}"')
 subprocess.Popen(f'explorer "{box[4]}"')
