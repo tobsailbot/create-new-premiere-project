ec19282fee0776e4ccd8ec62ea5186adba1c2f49
tobsailbot <tobsailbot@gmail.com>
2021-12-24 12:11:15 -0300

    new gui

---
+++ new_premiere_project_simpleGUI.py
@@ -0,0 +1,44 @@
+from ahk import AHK
+import os
+import sys
+import easygui as eg
+
+ahk = AHK()
+
+campos = ['Project Name', 'Project Path', 'Media Folder 1','Media Folder 2','Media Folder 3']
+datos = []
+default = ['',r'D:\TOBI-PC\Descargas\3-VIDEO PROJECTS',r'F:\VIDEO MEDIA\OBS VIDEOS',r'D:\TOBI-PC\Descargas\3-VIDEO PROJECTS\1.VIDEO EDITING RESOURCES\SOUNDS\SOUND FX',r'D:\TOBI-PC\Descargas\3-VIDEO PROJECTS\1.VIDEO EDITING RESOURCES\MEDIA\EMOJIS PNG']
+
+box = eg.multenterbox(msg='Enter the name and folders:',title='New Premiere Project',fields=campos, values=default)
+
+
+projName = box[0]
+Dir = box[1]
+projDir = Dir+f'\\{projName}'
+
+print(projName)
+print(projDir)
+
+os.makedirs(projDir)
+
+print(f'se creo el proyecto: {projName}')
+
+
+win = ahk.find_window(title=b'Adobe Premiere Pro')
+win.activate()
+
+ahk.send_input('^!n')
+ahk.type(projName)
+ahk.send_input('{Tab}{Tab}')
+ahk.send_input('{Enter}')
+ahk.type(projDir)
+ahk.send_input('{Enter}{Enter}')
+ahk.send_input('+{Tab}''+{Tab}''+{Tab}''+{Tab}')
+ahk.send_input('{Enter}')
+
+
+
+
+
+
+
